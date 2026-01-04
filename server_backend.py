3ff4dba44cc127cdcb585d8aba015051b0eb3249
Mrbanhamer <leonard@de-mare.se>
2026-01-04 18:22:43 +0100

    this version actually works

--- server_backend.py
+++ server_backend.py
@@ -1,13 +1,20 @@
 import mysql.connector
 
-namn = 'variables'
-
 mydb = mysql.connector.connect(
     host = 'localhost',
     user = 'app_user',
     password = 'password123',
     database = 'mydatabase'
 )
+
+def connection():
+    mydb = mysql.connector.connect(
+        host = 'localhost',
+        user = 'app_user',
+        password = 'password123',
+        database = 'mydatabase'
+    )
+    return mydb
 
 mycursor = mydb.cursor()
 
@@ -21,11 +28,15 @@ def make_database():
     )
 
 def add_new_data(name):
+    mydb = connection()
+    mycursor = mydb.cursor()
     mycursor.execute(
         'INSERT INTO namn (name) values (%s)',
-        (namn,)
+        (name,)
     )
     mydb.commit()
+    mycursor.close()
+    mydb.close()
 
 #mycursor.execute('SELECT * FROM namn')
 #rows = mycursor.fetchall()
